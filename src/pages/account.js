da84fc5b48c54dc82d82535f3e3c5117ee64b6b3
Oleg Zaporozhets <zaporozhets93@gmail.com>
2024-01-15 12:13:09 +0200

    add projects link

--- src/pages/account.js
+++ src/pages/account.js
@@ -8,7 +8,7 @@ const Page = () => (
   <>
     <Head>
       <title>
-        Account | Devias Kit
+        Account | Oleg Zaporozhets
       </title>
     </Head>
     <Box
@@ -22,7 +22,7 @@ const Page = () => (
         <Stack spacing={3}>
           <div>
             <Typography variant="h4">
-              Account
+              About me
             </Typography>
           </div>
           <div>
